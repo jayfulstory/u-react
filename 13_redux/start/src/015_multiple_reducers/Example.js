492de766bd02ff1000fcf63dd241881b6c4a40f8
kim <jayfulkr@gmail.com>
2023-06-03 17:45:48 +0900

    06/03 13

--- 13_redux/start/src/015_multiple_reducers/Example.js
+++ 13_redux/start/src/015_multiple_reducers/Example.js
@@ -1,7 +1,7 @@
-import Counter from "./components/Counter";
-import { CounterProvider } from "./context/CounterContext";
-import { Provider } from "react-redux";
-import store from "./store"
+import Counter from './components/Counter';
+import { CounterProvider } from './context/CounterContext';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const Example = () => {
   return (
